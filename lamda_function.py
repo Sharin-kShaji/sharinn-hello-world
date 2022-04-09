7be4d993cd490b7e578e8691dbc1718c59be918a
sharin k shaji <sharin.shaji@edstem.com>
2022-04-09 06:15:38 +0000

    Reading a file

--- lamda_function.py
+++ lamda_function.py
@@ -18,6 +18,10 @@ def lambda_handler(event, context):
         temp_file = "/tmp/hello_local.txt"
         s3.Bucket(BUCKET_NAME).download_file(KEY, temp_file)
         file = open(temp_file, "r")
+        return {
+            'statusCode' : 200,
+            'body' : file.read()
+        }
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
